88de25bb5a20452a84990919b5dd23c36194b94c
Christopher Pottes <christopherpottesc@gmail.com>
2022-01-10 11:23:01 -0300

    feat: add theme coc

---
+++ themes/coc/helpers/coc_helper.rb
@@ -0,0 +1,122 @@
+module CocHelper
+
+  def format_date(date, format=:short_custom, include_time=true)
+    if date.present?
+      if date.is_a?(Integer) && date.to_s.length == 13
+        value = Time.zone.at(date/1000)
+      else
+        value = Time.zone.at(date)
+      end
+      if include_time
+        I18n.l(value, format: format)
+      else
+        I18n.l(value.to_date, format: format)
+      end
+    else
+      nil
+    end
+  end
+
+  def recording_duration_secs(recording)
+    playbacks = recording[:playbacks]
+    valid_playbacks = playbacks.reject { |p| p[:type] == 'statistics' }
+    return 0 if valid_playbacks.empty?
+
+    len = valid_playbacks.first[:length]
+    return 0 if len.nil?
+
+    len * 60
+  end
+
+  def duration_in_hours_and_minutes(duration)
+    distance_of_time_in_hours_and_minutes(0, duration)
+  end
+
+  def distance_of_time_in_hours_and_minutes(from_time, to_time)
+    from_time = from_time.to_time if from_time.respond_to?(:to_time)
+    to_time = to_time.to_time if to_time.respond_to?(:to_time)
+    distance_in_hours   = (((to_time - from_time).abs) / 3600).floor
+    distance_in_minutes = ((((to_time - from_time).abs) % 3600) / 60).round
+
+    words = ''
+
+    if distance_in_hours > 0
+      words << I18n.t('helpers.distance_of_time_in_hours_and_minutes.hour', count: distance_in_hours)
+      if distance_in_minutes > 0
+        words << " #{I18n.t('helpers.distance_of_time_in_hours_and_minutes.connector')} "
+      end
+    end
+
+    if distance_in_minutes > 0
+      words << I18n.t('helpers.distance_of_time_in_hours_and_minutes.minute', count: distance_in_minutes)
+    end
+
+    words
+  end
+
+  def current_formatted_time_zone
+    ActiveSupport::TimeZone[Time.zone.name].to_s.gsub(/[^\s]*\//, '').gsub(/_/, ' ')
+  end
+
+  def get_custom_duration(duration)
+    duration_in_time = ScheduledMeeting.convert_duration_to_time(duration)
+    return time = duration_in_time[0].to_s + ':' + duration_in_time[1].to_s
+  end
+
+  def playback_url(room, record_id, playback)
+    if Rails.application.config.playback_url_authentication
+      recording_playback_url(room, record_id, playback[:type])
+    else
+      playback[:url]
+    end
+  end
+
+  def ext_elos_link(page)
+    link = links_external(page, I18n.locale)
+    link.nil? ? links_external(page, :en) : link
+  end
+
+  def links_external(page, locale)
+    links = {
+      terms: {
+        'en': 'https://elos.vc/site/en/terms',
+        'pt': 'https://elos.vc/site/pt/terms',
+      }
+    }
+    return links[page][locale]
+  end
+
+  def meeting_creator_name(meeting)
+    app_launch = AppLaunch.find_by(nonce: meeting.created_by_launch_nonce)
+    byebug
+    app_launch.omniauth_auth.dig('info', 'full_name')
+  end
+
+  def meeting_creator_by_current_user?(user, meeting)
+    byebug
+    app_launch = AppLaunch.find_by(nonce: meeting.created_by_launch_nonce)
+    user_creator_id = app_launch.omniauth_auth.dig('uid')
+    user.uid == user_creator_id ? true : false
+  end
+
+  def class_meeting_not_created_by_user(user, meeting, room)
+    byebug
+    user_creator = meeting_creator_by_current_user?(user, meeting)
+
+    if !user_creator && can_edit?(user, room)
+      return "user-not-creator-meeting"
+    elsif !user_creator && !can_edit?(user, room)
+      return ""
+    end
+  end
+
+  def show_divider_dropdown_menu?(user, meeting)
+    user_creator = meeting_creator_by_current_user?(user, meeting)
+    !meeting.disable_external_link && user_creator ? true : false
+  end
+
+  def badName
+    return true
+  end
+
+end
