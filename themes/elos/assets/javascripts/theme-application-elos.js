d8f66c5932dede646cd784679750216256d5ef80
wpramio <willianpramio@hotmail.com>
2022-01-12 18:52:54 -0300

    refactor: simplify the logic to keep meetings filters across reloads
    Add comments to the code

--- themes/elos/assets/javascripts/theme-application-elos.js
+++ themes/elos/assets/javascripts/theme-application-elos.js
@@ -57,36 +57,37 @@ $(document).on('turbolinks:load', function(){
     return true;
   });
 
+  // When the #meetings-filters radio input changes, adds or removes a class to
+  // #recording-table, which will hide or show meetings depending on the filter.
+  // Also adds the filter to the current URL, so it can be kept between page reloads.
   $("#meetings-filters input").on('change', function() {
     filter = $('input[name=filters]:checked', '#meetings-filters').val();
     switch (filter) {
       case 'recorded-only':
         $('#recording-table').addClass('filter-recorded-only');
-        if(window.history.state.filter != null)
-          window.history.replaceState({filter: "recorded-only"}, 'filter', '?filter=recorded-only')
-        else
-          window.history.pushState({filter: "recorded-only"}, 'filter', '?filter=recorded-only')
+        window.history.replaceState({filter: "recorded-only"}, null, '?filter=recorded-only');
         break;
       default:
         $('#recording-table').removeClass('filter-recorded-only');
-        window.history.replaceState({filter: "no-filters"}, 'filter', '?filter=no-filters')
+        window.history.replaceState(null, null, 'recordings');
     };
     return true;
   });
 
+  // On page load, if the URL contains a 'recorded-only' filter, checks the radio input
+  // and hides non-recorded meetings.
   if ((new URL(window.location.href)).searchParams.get("filter") == 'recorded-only' ) {
     $("#meetings-filters input[value=recorded-only]").attr("checked", true);
     $("#meetings-filters input[value=no-filters]").attr("checked", false);
     $('#recording-table').addClass('filter-recorded-only');
-    if(window.history.state.filter != null)
-      window.history.replaceState({filter: "recorded-only"}, 'filter', '?filter=recorded-only')
-    else
-      window.history.pushState({filter: "recorded-only"}, 'filter', '?filter=recorded-only')
+    window.history.replaceState({filter: "recorded-only"}, null, '?filter=recorded-only');
   }
 
+  // Adds the 'recorded-only' filter to recording edit links after click, to keep it
+  // after reload.
   $("#recording-table").on('click', function(e) {
     if (e.target.classList.contains('rec-edit')) {
-      if (window.history.state.filter != null && window.history.state.filter == 'recorded-only')
+      if (window.history.state.filter == 'recorded-only')
         e.target.href += '?filter=recorded-only';
     }
   });
