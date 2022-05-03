8657430d9966800dac11da566dbae034815ba51b
wpramio <willianpramio@hotmail.com>
2022-05-03 11:47:41 -0300

    chore: add coc_launch? method to AppLaunch
    It checks whether an AppLaunch is from COC portal.
    Add specs for it.

--- app/models/app_launch.rb
+++ app/models/app_launch.rb
@@ -30,6 +30,10 @@ class AppLaunch < ApplicationRecord
     params['custom_params']['tag']
   end
 
+  def coc_launch?
+    self.tag == 'coc'
+  end
+
   def user_params
     {
       uid: self.params['user_id'],
