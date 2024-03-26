10cbb4f217def8d009789675fbcfa641c347894d
wpramio <willianpramio@hotmail.com>
2024-03-26 19:48:33 -0300

    Version 0.15.0

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 
 module BbbAppRooms
   class Application < Rails::Application
-    VERSION = "0.14.1"
+    VERSION = "0.15.0"
 
     config.eager_load_paths << Rails.root.join('lib')
 
