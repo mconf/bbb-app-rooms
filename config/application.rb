bb332f377c47ba712c75b9125ac37de875e272b7
Guilherme Scariott <gringoscariott@gmail.com>
2024-04-04 17:17:38 -0300

    Version 0.16.0

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 
 module BbbAppRooms
   class Application < Rails::Application
-    VERSION = "0.15.0"
+    VERSION = "0.16.0"
 
     config.eager_load_paths << Rails.root.join('lib')
 
