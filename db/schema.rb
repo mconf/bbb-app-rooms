0a54ab662eab493c63bfa11ee222de7c843d0c65
Willian Pramio <willianpramio@hotmail.com>
2024-05-13 15:07:08 -0300

    feat: add `moodle_group_name` attribute to ScheduledMeeting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_01_181530) do
+ActiveRecord::Schema.define(version: 2024_05_10_113330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,6 +145,7 @@ ActiveRecord::Schema.define(version: 2024_04_01_181530) do
     t.boolean "disable_note", default: false
     t.string "hash_id"
     t.integer "moodle_group_id"
+    t.string "moodle_group_name"
     t.index ["created_by_launch_nonce"], name: "index_scheduled_meetings_on_created_by_launch_nonce"
     t.index ["hash_id"], name: "index_scheduled_meetings_on_hash_id", unique: true
     t.index ["repeat"], name: "index_scheduled_meetings_on_repeat"
