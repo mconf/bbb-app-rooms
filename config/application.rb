1ccc4382b8a97feace3114960182fa7f05f9aa1c
lucas-schell <lucas.schell@acad.pucrs.br>
2024-08-20 11:14:19 -0300

    Version 0.17.1

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 
 module BbbAppRooms
   class Application < Rails::Application
-    VERSION = "0.17.0"
+    VERSION = "0.17.1"
 
     config.eager_load_paths << Rails.root.join('lib')
 
