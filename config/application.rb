00a0ac363309ef4cfeee3fa12c68d3f39a83e94f
Christopher Pottes <christopherpottesc@gmail.com>
2024-10-21 17:24:44 -0300

    Version 0.19.0

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 
 module BbbAppRooms
   class Application < Rails::Application
-    VERSION = "0.18.2"
+    VERSION = "0.19.0"
 
     config.eager_load_paths << Rails.root.join('lib')
 
