0ada551f48266d8aeb1a6b69c1524f11bf226223
paolahoff <paolahoff00@gmail.com>
2024-10-23 14:08:01 -0300

    add: auxiliary functions for Room and ScheduledMeeting

--- app/models/scheduled_meeting.rb
+++ app/models/scheduled_meeting.rb
@@ -107,6 +107,14 @@ class ScheduledMeeting < ApplicationRecord
     repeat.present?
   end
 
+  def weekly?
+    recurring? && repeat == "weekly"
+  end
+
+  def every_two_weeks?
+    recurring? && repeat == "every_two_weeks"
+  end
+
   def meeting_id
     if room.moodle_group_select_enabled? && self.moodle_group_id.present?
       "#{room.meeting_id}-#{self.id}-#{self.moodle_group_id}"
