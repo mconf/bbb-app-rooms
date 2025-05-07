690166cfd251652e6b5746e19b07abecc42dea91
paolahoff <paolahoff00@gmail.com>
2025-05-07 12:20:24 -0300

    migrate: changes eduplay token column type
    from `string` type to `token` type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_21_171714) do
+ActiveRecord::Schema.define(version: 2025_04_28_125814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 2024_10_21_171714) do
 
   create_table "eduplay_tokens", force: :cascade do |t|
     t.string "user_uid"
-    t.string "token"
+    t.text "token"
     t.datetime "expires_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
