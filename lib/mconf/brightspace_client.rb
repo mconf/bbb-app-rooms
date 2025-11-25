938a7e639993d18a7888e07ba8bbeea17f547fbc
Willian Pramio <willianpramio@hotmail.com>
2025-11-25 17:33:11 -0300

    refactor(brightspace_client): enhance logging format

--- lib/mconf/brightspace_client.rb
+++ lib/mconf/brightspace_client.rb
@@ -14,7 +14,9 @@ module Mconf
       @access_token = access_token
       # the supported API versions can be found at "#{@base_url}/d2l/api/versions/"
       @api_versions = api_versions || { lp: '1.54', le: '1.89' }
-      @user_info_str = "(email: #{user_info[:email]}, launch_nonce: #{user_info[:launch_nonce]})" unless user_info.blank?
+      @user_info_str = ""
+      @user_info_str = "[email=#{user_info[:email]}, launch_nonce=#{user_info[:launch_nonce]}]" if user_info.present?
+      Rails.logger.info "[BrightspaceClient] Initialized with base_url=#{@base_url} #{@user_info_str}"
     end
 
     # Fetches the profile image of the authenticated user
@@ -22,18 +24,18 @@ module Mconf
     # @return [String, nil] binary data of the profile image, or nil if not found
     def get_profile_image(size: 200)
       url = "#{@base_url}/d2l/api/lp/#{@api_versions[:lp]}/profile/myProfile/image?size=#{size}"
-      Rails.logger.info "Calling Brightspace API at #{url} to get user profile image #{@user_info_str}"
+      Rails.logger.info "[BrightspaceClient] Calling API at #{url} to get user profile image #{@user_info_str}"
 
       res = RestClient.get(url, self.http_headers)
       res.body
     rescue RestClient::NotFound => e
-      Rails.logger.warn "Profile image not found in Brightspace: #{e.message}"
+      Rails.logger.info "[BrightspaceClient]#{@user_info_str} Profile image not found, response: #{e.message}"
       nil
     rescue RestClient::ExceptionWithResponse => e
-      Rails.logger.error "Error calling Brightspace API: #{e.response}"
+      Rails.logger.error "[BrightspaceClient##{__method__}]#{@user_info_str} RestClient error: #{e.response}"
       nil
     rescue StandardError => e
-      Rails.logger.error "Unexpected error calling Brightspace API: #{e.message}"
+      Rails.logger.error "[BrightspaceClient##{__method__}]#{@user_info_str} Unexpected error: #{e.message}"
       nil
     end
 
