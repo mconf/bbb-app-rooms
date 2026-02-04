a0f2925fbd30c84b89cfab7fb975c62abeef71ca
Willian Pramio <willianpramio@hotmail.com>
2026-02-04 15:25:55 -0300

    Version 1.2.0

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 
 module BbbAppRooms
   class Application < Rails::Application
-    VERSION = "1.1.1"
+    VERSION = "1.2.0"
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
