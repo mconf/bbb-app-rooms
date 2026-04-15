6b397c2cf18a6d9fabee93e827b4287bceeee9ae
Christopher Pottes <33357698+christopherpottesc@users.noreply.github.com>
2026-04-15 10:53:44 -0300

    refactor: remove rnp_chat_id config (Blip chat replaced by Ipezinho)

--- config/application.rb
+++ config/application.rb
@@ -141,7 +141,6 @@ module BbbAppRooms
     config.filesender_client_secret     = Mconf::Env.fetch('MCONF_FILESENDER_CLIENT_SECRET')
 
     # RNP CHAT
-    config.rnp_chat_id = Mconf::Env.fetch('RNP_CHAT_ID', '')
 
     # Moodle API
     config.moodle_api_timeout = Mconf::Env.fetch_int('MCONF_MOODLE_API_TIMEOUT', 5)
